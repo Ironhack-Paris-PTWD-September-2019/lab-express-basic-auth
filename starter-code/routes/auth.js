9e43a2f4eb668fda8c780d945e34ae42150c571f
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2019-12-10 21:04:40 +0100

    Iteration 2

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -12,8 +12,7 @@ router.get('/signup', (req,res,next)=>{
 });
 
 router.post('/signup', (req,res,next)=>{
-  const username=req.body.username; 
-  const password=req.body.password;
+  const {username,password} =req.body; 
 
   if(!username || !password){
     res.render('auth/signup', {errorM:"You have to enter a Username and a Password"})
@@ -43,7 +42,41 @@ router.post('/signup', (req,res,next)=>{
   }).catch(err=>{
     console.error(err);
     next(err);
-  })
+  });
+
+});
+
+router.get('/login', (req,res,next)=>{
+
+  res.render('auth/login'); 
+
+});
+
+router.post('/login', (req,res,next)=>{
+  const {username,password} = req.body; 
+
+  if(!username || !password) {
+    res.render('/login', {errorM:"You have to fill in Username and Password for login"})
+    return;
+  }
+
+  User.findOne({username:username}).then(user=>{
+    if(!user) {
+      res.render('/login', {errorM:"Username or Password Incorrect"}); 
+      return;
+    }
+
+    if(!bcrypt.compareSync(password,user.password)){
+      res.render('/login', {errorM:"You have to fill in Username and Password for login"});
+      return;
+    }
+    req.session.currentUser = user; 
+    res.redirect('/');
+
+  }).catch(err=>{
+    console.error(err);
+    next(err);
+  });
 
 });
 
