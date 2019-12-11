b8e20ffcf1c2439fe4550e392a4b886240c5cb45
blablamedia <pierre@gambaud.com>
2019-12-11 21:22:55 +0100

    iteration 2: done

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -5,6 +5,16 @@ const User = require(`../models/user`);
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+
+// ######  ####  ######   ##    ## ##     ## ########  
+// ##    ##  ##  ##    ##  ###   ## ##     ## ##     ## 
+// ##        ##  ##        ####  ## ##     ## ##     ## 
+//  ######   ##  ##   #### ## ## ## ##     ## ########  
+//       ##  ##  ##    ##  ##  #### ##     ## ##        
+// ##    ##  ##  ##    ##  ##   ### ##     ## ##        
+//  ######  ####  ######   ##    ##  #######  ##        
+
+
 router.get(`/signup`, (req,res,next) => {
   res.render(`auth/signup`)
 })
@@ -42,5 +52,55 @@ router.post(`/signup`, (req,res,next) => {
   })
   .catch(err => next(err))  
 })
+
+
+// ##        #######   ######   #### ##    ## 
+// ##       ##     ## ##    ##   ##  ###   ## 
+// ##       ##     ## ##         ##  ####  ## 
+// ##       ##     ## ##   ####  ##  ## ## ## 
+// ##       ##     ## ##    ##   ##  ##  #### 
+// ##       ##     ## ##    ##   ##  ##   ### 
+// ########  #######   ######   #### ##    ## 
+
+
+router.get(`/login`, (req,res,next) => {
+  res.render(`auth/login`)
+})
+
+router.post(`/login`, (req,res,next) => {
+  const { username, password } = req.body;
+
+  if(username.length <= 0 || password.length <= 0) {
+    res.render(`auth/login`, {
+      errorMessage: `Please fill both username and password inputs`
+    });
+    return;
+  }
+
+  User.findOne({username: username})
+  .then(user => {
+    if(!user) {
+      res.render(`auth/login`, {
+        errorMessage: `The username "${username}" does not exist`
+      });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    if(bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res.redirect(`/`)
+    } else {
+      res.render(`auth/login`, {
+        username: username,
+        errorMessage: `Username and password do not match`
+      });
+    }
+  })
+  .catch(err => next(err))
+})
+
 
 module.exports = router;
